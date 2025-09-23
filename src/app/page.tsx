e99994686e983ab068030294ee02d44b45587a1f
Daniel Sângelo <danielsangelo@hotmail.com>
2025-09-22 22:03:55 -0300

    feat: add header and banner. #FSW-0008

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <Button> Teste </Button>
-  );
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Daniel </h2>
+        <p>Segunda-feira, 05 de Agosto de 2025</p>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+            alt="banner"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
